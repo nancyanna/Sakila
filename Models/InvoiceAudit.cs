2bd5547563846d5d9dcb46bd4c7d397d7ae61ce6
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-04-25 13:21:59 -0500

    Add files via upload

---
+++ Models/InvoiceAudit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sakila1.Models
+{
+    public partial class InvoiceAudit
+    {
+        public int InvoiceAuditId { get; set; }
+        public int? InvoiceId { get; set; }
+        public string Username { get; set; }
+        public DateTime? DateAdded { get; set; }
+    }
+}
---
+++ Models/Salary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sakila1.Models
+{
+    public partial class Salary
+    {
+        public int SalaryId { get; set; }
+        public int EmployeeNum { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime EffectiveDate { get; set; }
+    }
+}
